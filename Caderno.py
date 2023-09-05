16581e56ce81e67cfeae71d8ab1e411a63f32dfc
Ricardo6664 <ricardo_rg78@hotmail.com>
2023-09-05 16:29:12 -0300

    finalizado 2

--- Caderno.py
+++ Caderno.py
@@ -1,6 +1,6 @@
 """Este é o primeiro caderno criado no git"""
 print("hello world")
-
+print('Meu nome é ricardo')
 """Para criar use o prefico code (Editor de código escolhido),
 depois defina o nome + extesão do arquivo
 Porem para visualizar modificações nele, deve add no git
